aa57d97a70b7891225d4a1adc7b676e7a3230df5
Nicholas Merolle <nmerolle11@hotmail.com>
2020-11-29 05:07:49 +0000

    created_base_files

---
+++ dnd5e_class_spells/lib/dnd5e_class_spells.rb
@@ -0,0 +1,6 @@
+require "dnd5e_class_spells/version"
+
+module Dnd5eClassSpells
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ dnd5e_class_spells/lib/dnd5e_class_spells/version.rb
@@ -0,0 +1,3 @@
+module Dnd5eClassSpells
+  VERSION = "0.1.0"
+end
