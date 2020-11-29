fe1085da6d8650fd8e1bf9605c5237b3fcaedfaf
Nicholas Merolle <nmerolle11@hotmail.com>
2020-11-29 07:05:05 +0000

    Automatically backed up by Learn

--- dnd5e_class_spells/lib/environment.rb
+++ dnd5e_class_spells/lib/environment.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 require_relative "./dnd5e_class_spells/version"
-require_relative "./cli.rb"
+require_relative "./dnd5e_class_spells/cli.rb"
 require_relative "./api.rb"
 require_relative "./klass.rb"
 require_relative "./spell.rb"
