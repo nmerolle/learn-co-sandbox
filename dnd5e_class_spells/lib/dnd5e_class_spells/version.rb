51a8d0fec8cf91913c06d3f997c0563d1b2506bb
Nicholas Merolle <nmerolle11@hotmail.com>
2021-07-04 01:19:29 +0000

    Automatically backed up by Learn

--- dnd5e_class_spells/lib/dnd5e_class_spells/version.rb
+++
@@ -1,3 +0,0 @@
-module Dnd5eClassSpells
-  VERSION = "0.1.0"
-end
--- dnd5e_class_spells/lib/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'pry'
-require 'rest-client'
-
-require_relative "./dnd5e_class_spells/version"
-require_relative "./dnd5e_class_spells/cli.rb"
-require_relative "./dnd5e_class_spells/api.rb"
-require_relative "./dnd5e_class_spells/klass.rb"
-require_relative "./dnd5e_class_spells/spell.rb"
-
